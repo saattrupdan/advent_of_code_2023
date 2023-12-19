b6768077d0dd36631ef7bfcf4e3d70ebc4d42fec
saattrupdan <saattrupdan@gmail.com>
2023-12-19 23:09:02 +0100

    feat: Finish first part of day 10

--- src/bin/day10-1.rs
+++ src/bin/day10-1.rs
@@ -118,7 +118,113 @@
 // Find the single giant loop starting at S. How many steps along the loop does it take
 // to get from the starting position to the point farthest from the starting position?
 
+use std::collections::HashMap;
+
 fn main() {
-    // let data = include_str!("../../data/day10.txt");
-    // println!("The answer for the first task is: {}", answer);
+    let data = include_str!("../../data/day10.txt");
+
+    // Parse data as a matrix
+    let matrix = data
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    // Find the coordinates of the starting point
+    let mut start = (0, 0);
+    for (i, row) in matrix.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if *col == 'S' {
+                start = (i, j);
+            }
+        }
+    }
+
+    let mut valid_neighbour_map: HashMap<usize, Vec<(usize, usize)>>;
+    let mut paths: HashMap<usize, Vec<(usize, usize)>> = {
+        let mut paths: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
+        paths.insert(0, vec![start]);
+        paths
+    };
+    let mut previous: Option<(usize, usize)>;
+    let mut new_path_idx = 1;
+
+    while !paths
+        .values()
+        .any(|path| path.last().is_some() && path.last().unwrap() == &start && path.len() > 1)
+    {
+        valid_neighbour_map = HashMap::new();
+        for (path_idx, path) in paths.iter() {
+            if path.len() > 1 {
+                previous = Some(path[path.len() - 2]);
+            } else {
+                previous = None;
+            }
+            let current: (usize, usize) = path[path.len() - 1];
+
+            // Determine the directions that the current node can be connected
+            let can_be_connected_north = current.0 > 0
+                && ['S', 'F', '7', '|'].contains(&matrix[current.0 - 1][current.1])
+                && ['S', 'J', 'L', '|'].contains(&matrix[current.0][current.1]);
+            let can_be_connected_east = current.1 < matrix[0].len() - 1
+                && ['S', 'J', '7', '-'].contains(&matrix[current.0][current.1 + 1])
+                && ['S', 'F', 'L', '-'].contains(&matrix[current.0][current.1]);
+            let can_be_connected_south = current.0 < matrix.len() - 1
+                && ['S', 'L', 'J', '|'].contains(&matrix[current.0 + 1][current.1])
+                && ['S', 'F', '7', '|'].contains(&matrix[current.0][current.1]);
+            let can_be_connected_west = current.1 > 0
+                && ['S', 'L', 'F', '-'].contains(&matrix[current.0][current.1 - 1])
+                && ['S', 'J', '7', '-'].contains(&matrix[current.0][current.1]);
+
+            // Find the valid neighbours
+            let mut valid_neighbours: Vec<(usize, usize)> = Vec::new();
+            if can_be_connected_north {
+                valid_neighbours.push((current.0 - 1, current.1));
+            }
+            if can_be_connected_east {
+                valid_neighbours.push((current.0, current.1 + 1));
+            }
+            if can_be_connected_south {
+                valid_neighbours.push((current.0 + 1, current.1));
+            }
+            if can_be_connected_west {
+                valid_neighbours.push((current.0, current.1 - 1));
+            }
+
+            // Ensure that the previous node is not in the valid neighbours
+            if previous.is_some() {
+                if valid_neighbours.contains(&previous.unwrap()) {
+                    valid_neighbours.retain(|&n| n != previous.unwrap());
+                }
+            }
+
+            // Add the valid neighbours to the map, if any
+            if valid_neighbours.len() > 0 {
+                valid_neighbour_map.insert(*path_idx, valid_neighbours);
+            }
+        }
+
+        // For each valid neighbour, create a new path
+        for (path_idx, valid_neighbours) in valid_neighbour_map.iter() {
+            let old_path: Vec<(usize, usize)> = paths[path_idx].clone();
+            for valid_neighbour in valid_neighbours.iter() {
+                let mut new_path = old_path.clone();
+                new_path.push(*valid_neighbour);
+                new_path_idx += 1;
+                paths.insert(new_path_idx, new_path);
+            }
+            paths.remove(path_idx);
+        }
+    }
+
+    let giant_loop = paths
+        .values()
+        .find(|path| path.last().is_some() && path.last().unwrap() == &start && path.len() > 1)
+        .unwrap();
+
+    // Remove last element, which is the starting point
+    let giant_loop = &giant_loop[..giant_loop.len() - 1];
+
+    // The answer is the half of the length of the giant loop, rounded down
+    let answer = (giant_loop.len() as f32 / 2.0).floor();
+    println!("The answer for the first task is: {}", answer);
 }
