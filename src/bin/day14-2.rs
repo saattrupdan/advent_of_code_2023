b4910d1bdd313c0ce2a346e069461c0609883e2e
saattrupdan <saattrupdan@gmail.com>
2023-12-20 17:02:06 +0100

    chore: Add placeholders for days 13-20

---
+++ src/bin/day14-2.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // let data = include_str!("../../data/day14.txt");
+    // println!("The answer for the second task is: {}", answer);
+}
---
+++ src/bin/day16-2.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // let data = include_str!("../../data/day16.txt");
+    // println!("The answer for the second task is: {}", answer);
+}
